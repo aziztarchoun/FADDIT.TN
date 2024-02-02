62eae4f7b81242cb41a4c209bd59636e09d62443
tarchouuun <zizotarchoun94@gmail.com>
2024-02-02 18:24:17 +0100

    Update page.jsx and package.json

--- ouuuh/app/about/page.jsx
+++ ouuuh/app/about/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <main className="flex flex-col items-center justify-between min-w-screen bg-gradient-to-r from-cyan-500 to-blue-500">
             <div className="flex h-screen items-center justify-center">
             <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-                <h1 className='text-5xl text-white'>allah ilahina fi sramna</h1>
+                <h1 className='text-5xl text-white'>allah ilahina fi hwayejna</h1>
             </div>
             </div>
         </main>
